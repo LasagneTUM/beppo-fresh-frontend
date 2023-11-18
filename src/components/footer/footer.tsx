645be7d8d4f2fc39538d4d74329cc98133afe733
Leonard Goldstein <43523968+goldst@users.noreply.github.com>
2023-11-18 17:47:31 +0100

    Continue detail page

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,10 @@
+import styles from "./footer.module.css";
+
+export default function Footer() {
+
+  return (
+    <footer className={styles.footer}>
+      © 2023 LasagnaTUM for HelloFresh at HackaTUM
+    </footer>
+  );
+}
