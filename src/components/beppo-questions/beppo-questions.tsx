7199cb098c179d953493b3b6be5f76e8515a53f0
Leonard Goldstein <43523968+goldst@users.noreply.github.com>
2023-11-18 15:00:36 +0100

    Beppo 3

--- src/components/beppo-questions/beppo-questions.tsx
+++ src/components/beppo-questions/beppo-questions.tsx
@@ -4,18 +4,18 @@ export function BeppoQuestions() {
   return (
     <div className={styles.beppoQuestions}>
       <div className={styles.speechBubble}>
+        <div className={styles.x}>x</div>
         <div className={styles.preQuestion}>Help me to find better recommendations :)</div>
-        <h2>What do you like more?</h2>
-        <div>x</div>
-        <div>
+        <h2 className={styles.question}>What do you like more?</h2>
+        <div className={styles.buttons}>
           <button>1</button>
           <button>2</button>
           <button>3</button>
           <button>4</button>
           <button>5</button>
         </div>
-        <div>Text 1</div>
-        <div>Text 5</div>
+        <div className={styles.text1}>Text 1</div>
+        <div className={styles.text5}>Text 5</div>
       </div>
     </div>
   );
