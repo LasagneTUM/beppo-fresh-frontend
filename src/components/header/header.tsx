16b4f36e00fdbc102d7cc9c444374f3776dba0c9
Leonard Goldstein <43523968+goldst@users.noreply.github.com>
2023-11-18 13:49:01 +0100

    header stuff

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,17 +1,29 @@
-// import { useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import Button from "../button/button";
 import styles from "./header.module.css";
 import { useUser } from "../../hooks/use-user";
+import logo from './logo.png';
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  const [user, setUser] = useUser();
+  const [, setUser] = useUser();
+  const navigate = useNavigate();
 
   const logout = () => {
     setUser(null);
-    // navigate("/login");
+    navigate("/login");
   };
   return (
     <div className={styles.header}>
+      <div className={styles.headerLeft}>
+        <Link to="/">
+          <img src={logo} alt="HelloFresh Logo" className={styles.logo} />
+        </Link>
+        <span className={styles.menuItem}>Kochboxen</span>
+        <span className={styles.menuItem}>So funktioniert's</span>
+        <span className={styles.menuItem}>Rezepte</span>
+        <span className={styles.menuItem}>Geschenkgutschein</span>
+      </div>
       <Button variant="secondary" onClick={logout} className={styles.logoutBtn}>
         Logout
       </Button>
