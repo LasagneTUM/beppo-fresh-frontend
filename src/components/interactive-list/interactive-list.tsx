5e6bb91386081f00e6457cd820b2d89e05508d20
Alexander Liebald <alexander.liebald@gmail.com>
2023-11-19 09:55:05 +0100

    Update description text

--- src/components/interactive-list/interactive-list.tsx
+++ src/components/interactive-list/interactive-list.tsx
@@ -61,10 +61,9 @@ export default function InteractiveList() {
     <div className={styles.container}>
       <h1 className={styles.title}>Recipe finder</h1>
       <p className={styles.description}>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren
+        What do you want to eat next week? Discover meals from all around the
+        world. If you answer some questions about your preferences you might
+        find some hidden gems for you.
       </p>
       {/* <Button onClick={() => setRecipes(shuffleArray([...recipes]))}>Shuffle</Button> */}
       <BeppoQuestions onSelectionDone={onBeppoSelection} />
