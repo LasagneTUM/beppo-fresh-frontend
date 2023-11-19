5dcc333bfe8037e443c6729b4ea7491e3df37a79
Alexander Liebald <alexander.liebald@gmail.com>
2023-11-19 04:16:28 +0100

    Fix linter error

--- src/components/recipe-page/recipe-page.tsx
+++ src/components/recipe-page/recipe-page.tsx
@@ -13,7 +13,7 @@ import {
   Viewport
 } from '@radix-ui/react-toast';
 import { useState } from 'react';
-import { useRecipe } from '../../network/useRecipe';
+// import { useRecipe } from '../../network/useRecipe';
 import { Navigate } from 'react-router';
 
 export function RecipePage() {
@@ -22,7 +22,7 @@ export function RecipePage() {
     setOpen(true);
   };
 
-  const recipe = useRecipe();
+  // const recipe = useRecipe();
 
   const [goHome, setGoHome] = useState(false);
   if (goHome) {
