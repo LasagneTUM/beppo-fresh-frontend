7e5e4b86c66e1ca30766968c5921001f04c49e03
Alexander Liebald <alexander.liebald@gmail.com>
2023-11-19 08:58:21 +0100

    Remove trailing space from base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/beppo-fresh-frontend/ ",
+  base: "/beppo-fresh-frontend/",
 });
