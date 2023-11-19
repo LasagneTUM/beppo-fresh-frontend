609ee378a39e0dd869dfa047c879a83e42104093
Alexander Liebald <alexander.liebald@gmail.com>
2023-11-19 09:01:17 +0100

    Check mode for base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const mode = process.env.APP_ENV;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/beppo-fresh-frontend/",
+  base: mode === "production" ? "/beppo-fresh-frontend/" : undefined,
 });
